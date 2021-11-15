cd6e878eb320df54d84ebbc4db50793f16cb9ab7
bryansilvagit <basp.95@outlook.com>
2021-11-15 14:09:59 -0500

    Add resources

---
+++ src/main/resources/PICA - Identity-Provider-DB.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `pica`.`user` (
+  `user_id` VARCHAR(150) NOT NULL,
+  `username` VARCHAR(45) NOT NULL,
+  `name` VARCHAR(150) NOT NULL,
+  `last_name` VARCHAR(150) NOT NULL,
+  `password` VARCHAR(150) NOT NULL,
+  `email` VARCHAR(150) NOT NULL,
+  `status` VARCHAR(45) NOT NULL,
+  `rol` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`user_id`));
